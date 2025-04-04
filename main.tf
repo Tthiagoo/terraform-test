6ebb161b0d48328798a6d329f0f7cfdb10d01e05
Thiago Pereira <51219408+Tthiagoo@users.noreply.github.com>
2025-04-04 14:02:58 -0300

    primeiro recurso

---
+++ main.tf
@@ -0,0 +1,9 @@
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "thiago-bucket-iac"
+
+  tags = {
+    Name    = "Primeiro bucket"
+    Iac     = "true"
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.36.0"
+    }
+  }
+}
+provider "aws" {}
